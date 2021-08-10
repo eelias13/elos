b3669864b0e6d76d297a745c869a2256dac9d485
elias <maierelias13@gmail.com>
2021-08-10 13:02:02 +0200

    adding test

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
-// #![feature(asm)] // enables asm! macro
+
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(elos::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-mod vga_buffer;
+use elos::println;
 
 #[no_mangle]
-pub extern "C" fn _start() {
+pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("Some panic message"); 
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
 
 /// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    elos::test_panic_handler(info)
 }
