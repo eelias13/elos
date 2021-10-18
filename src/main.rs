d4a80f59c701ac4fa127231a16cff81de936a615
elias <maierelias13@gmail.com>
2021-10-18 14:33:06 +0200

    handling `double fault exceptions`

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,7 @@ use elos::println;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-
     elos::init();
-    x86_64::instructions::interrupts::int3();
 
     #[cfg(test)]
     test_main();
