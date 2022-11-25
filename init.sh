460531bfea831b1a1df1a3c550e494743a27e386
Elias Maier <maierelias13@gmai.com>
2022-11-25 21:24:18 +0100

    update init

--- init.sh
+++ init.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-sudo apt install qemu-system-x86 -y
+
+# install qemu (the emulator) and curl neded for cargo
+apt install curl qemu-system-x86 -y
+
+# install rust
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+# install rust compiler tools
 rustup component add rust-src --toolchain nightly-2021-07-27-x86_64-unknown-linux-gnu
 rustup component add llvm-tools-preview
 rustup update
+
+# install bootimage
+cargo install bootimage
